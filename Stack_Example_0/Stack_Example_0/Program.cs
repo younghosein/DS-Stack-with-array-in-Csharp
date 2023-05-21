e010b723604ed35a2fc8712216b25d9366cb6e78
Hosein Mohamadpor <83702864+younghosein@users.noreply.github.com>
2023-05-22 01:21:35 +0330

    Add files via upload

---
+++ Stack_Example_0/Stack_Example_0/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Example_0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack S = new Stack(10);
+
+            S.push(13);
+            S.push(93);
+            S.push(65);
+            S.push(28);
+            S.push(71);
+            S.push(92);
+            S.push(11);
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Stack Is : ");
+            S.print();
+            Console.ResetColor();
+            Console.WriteLine("\n");
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            S.pop();
+            S.pop();
+            S.push(65);
+            Console.WriteLine("Stack Is : ");
+            S.print();
+            Console.ResetColor();
+            Console.WriteLine("\n");
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Top Is : ");
+            int p = S.peek();
+            Console.WriteLine(p);
+            Console.ResetColor();
+            Console.WriteLine();
+
+            S.removeDoubles(S);
+            Console.WriteLine("Stack Is : ");
+            S.print();
+            Console.ResetColor();
+            Console.WriteLine("\n");
+
+
+            //Console.ForegroundColor = ConsoleColor.Green;
+            //int a = S.avg();
+            //Console.WriteLine("Average Is : ");
+            //Console.WriteLine(a);
+            //Console.ResetColor();
+            //Console.WriteLine("\n");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Stack_Example_0/Stack_Example_0/Stack.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Example_0
+{
+    class Stack
+    {
+        public int[] s;
+        public int top;
+        public int max;
+        public Stack(int size)
+        {
+            s = new int[size];
+            top = -1;
+            max = size;
+        }
+        public void print()
+        {
+            if (top == -1)
+            {
+                Console.WriteLine("Stack is Empty");
+                return;
+            }
+            else
+            {
+                for (int i = 0; i <= top; i++)
+                {
+                    Console.Write(s[i] + " . ");
+                }
+            }
+        }
+        public void push(int data)
+        {
+            if (top == max - 1)
+            {
+                Console.WriteLine("Stack Is Full !");
+                return;
+            }
+            else
+            {
+                s[++top] = data;
+            }
+        }
+
+        public int pop()
+        {
+            if (top == -1)
+            {
+                Console.WriteLine("Stack is Empty !");
+                return -1;
+            }
+            else
+            {
+                Console.WriteLine($"{s[top]} Is Deleted...");
+                return s[top--];
+            }
+        }
+
+        public int peek()
+        {
+            if (top == -1)
+            {
+                Console.WriteLine("Stack is Empty !");
+                return -1;
+            }
+            else
+            {
+                return s[top];
+            }
+        }
+        public int avg()
+        {
+            int sum = 0;
+            int c = 0;
+            int avg;
+            if (top == -1)
+            {
+                Console.WriteLine("Stack is Empty");
+                return -1;
+            }
+            else
+            {
+                for (int i = 0; i <= top; i++)
+                {
+                    sum += s[i];
+                    c++;
+                }
+                Console.WriteLine($"Sum Is : {sum}");
+                Console.WriteLine($"Count Is : {c}");
+                avg = sum / c;
+                return avg;
+            }
+        }
+    }
+}
